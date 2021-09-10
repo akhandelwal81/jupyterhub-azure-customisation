6b78c2770c65ef289b2723e0b8338d9682da9d20
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-11 00:42:24 +0100

    API request

--- jupyterhub/services/auth.py
+++ jupyterhub/services/auth.py
@@ -305,3 +305,106 @@ class HubAuth(SingletonConfigurable):
         if service_name:
             return {f'access:services!service={service_name}'}
         return set()
+    def _check_hub_authorization(self, url, api_token, cache_key=None, use_cache=True):
+        """Identify a user with the Hub
+        Args:
+            url (str): The API URL to check the Hub for authorization
+                       (e.g. http://127.0.0.1:8081/hub/api/user)
+            cache_key (str): The key for checking the cache
+            use_cache (bool): Specify use_cache=False to skip cached cookie values (default: True)
+        Returns:
+            user_model (dict): The user model, if a user is identified, None if authentication fails.
+        Raises an HTTPError if the request failed for a reason other than no such user.
+        """
+        if use_cache:
+            if cache_key is None:
+                raise ValueError("cache_key is required when using cache")
+            # check for a cached reply, so we don't check with the Hub if we don't have to
+            try:
+                return self.cache[cache_key]
+            except KeyError:
+                app_log.debug("HubAuth cache miss: %s", cache_key)
+
+        data = self._api_request(
+            'GET',
+            url,
+            headers={"Authorization": "token " + api_token},
+            allow_403=True,
+        )
+        if data is None:
+            app_log.warning("No Hub user identified for request")
+        else:
+            app_log.debug("Received request from Hub user %s", data)
+        if use_cache:
+            # cache result
+            self.cache[cache_key] = data
+        return data
+
+    def _api_request(self, method, url, **kwargs):
+        """Make an API request"""
+        allow_403 = kwargs.pop('allow_403', False)
+        headers = kwargs.setdefault('headers', {})
+        headers.setdefault('Authorization', 'token %s' % self.api_token)
+        if "cert" not in kwargs and self.certfile and self.keyfile:
+            kwargs["cert"] = (self.certfile, self.keyfile)
+            if self.client_ca:
+                kwargs["verify"] = self.client_ca
+        try:
+            r = requests.request(method, url, **kwargs)
+        except requests.ConnectionError as e:
+            app_log.error("Error connecting to %s: %s", self.api_url, e)
+            msg = "Failed to connect to Hub API at %r." % self.api_url
+            msg += (
+                "  Is the Hub accessible at this URL (from host: %s)?"
+                % socket.gethostname()
+            )
+            if '127.0.0.1' in self.api_url:
+                msg += (
+                    "  Make sure to set c.JupyterHub.hub_ip to an IP accessible to"
+                    + " single-user servers if the servers are not on the same host as the Hub."
+                )
+            raise HTTPError(500, msg)
+
+        data = None
+        if r.status_code == 403 and allow_403:
+            pass
+        elif r.status_code == 403:
+            app_log.error(
+                "I don't have permission to check authorization with JupyterHub, my auth token may have expired: [%i] %s",
+                r.status_code,
+                r.reason,
+            )
+            app_log.error(r.text)
+            raise HTTPError(
+                500, "Permission failure checking authorization, I may need a new token"
+            )
+        elif r.status_code >= 500:
+            app_log.error(
+                "Upstream failure verifying auth token: [%i] %s",
+                r.status_code,
+                r.reason,
+            )
+            app_log.error(r.text)
+            raise HTTPError(502, "Failed to check authorization (upstream problem)")
+        elif r.status_code >= 400:
+            app_log.warning(
+                "Failed to check authorization: [%i] %s", r.status_code, r.reason
+            )
+            app_log.warning(r.text)
+            msg = "Failed to check authorization"
+            # pass on error from oauth failure
+            try:
+                response = r.json()
+                # prefer more specific 'error_description', fallback to 'error'
+                description = response.get(
+                    "error_description", response.get("error", "Unknown error")
+                )
+            except Exception:
+                pass
+            else:
+                msg += ": " + description
+            raise HTTPError(500, msg)
+        else:
+            data = r.json()
+
+        return data
